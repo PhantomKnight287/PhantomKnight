c14ca2bd87b60b25ad6bd21f4337e2e9f9118a44
PhantomKnight287 <gillgurpal699@gmail.com>
2021-12-19 16:56:41 +0530

    add context provider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,18 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-
 import dynamic from "next/dynamic";
 import Navbar from "../components/Navbar/Navbar";
+import { UserStateProvider } from "../context";
 
 const Footer = dynamic(() => import("../components/footer/Footer"), {
     loading: () => <p>loading....</p>
 });
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Navbar />
-            <Component {...pageProps} />
-            <Footer />
+            <UserStateProvider>
+                <Navbar />
+                <Component {...pageProps} />
+                <Footer />
+            </UserStateProvider>
         </>
     );
 }
