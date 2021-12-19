0f6ad433299503c1a5185dabfde684494fd41010
PhantomKnight287 <gillgurpal699@gmail.com>
2021-12-19 16:55:51 +0530

    create react context for userState

---
+++ client/context/userContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, Dispatch, useContext, useReducer } from "react";
+import { userContext } from "../types";
+const UserStateContext = createContext<userContext>({
+    avatar: "",
+    username: "",
+    id: "",
+    discriminator: "",
+    email: ""
+});
+
+const UserStateDispatchContext = createContext(
+    {} as Dispatch<{ type: string; payload: userContext }>
+);
+
+const reducer = (
+    state: userContext,
+    action: { type: string; payload: userContext }
+) => {
+    switch (action.type) {
+        case "SET_USER":
+            return {
+                ...state,
+                ...action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export const UserStateProvider = ({
+    children
+}: {
+    children: JSX.Element[];
+}) => {
+    const [userState, dispatch] = useReducer(reducer, {
+        avatar: "",
+        username: "",
+        id: "",
+        discriminator: "",
+        email: ""
+    });
+    return (
+        <UserStateContext.Provider value={userState}>
+            <UserStateDispatchContext.Provider value={dispatch}>
+                {children}
+            </UserStateDispatchContext.Provider>
+        </UserStateContext.Provider>
+    );
+};
+
+export const useUserState = () => useContext(UserStateContext);
+export const useUserStateDispatch = () => useContext(UserStateDispatchContext);
