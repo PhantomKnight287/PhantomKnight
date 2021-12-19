ccc5ce65e49a5cf8e932d7a66416422bd99b464f
PhantomKnight287 <gillgurpal699@gmail.com>
2021-12-19 16:55:27 +0530

    add constants

---
+++ client/constants/index.ts
@@ -0,0 +1,12 @@
+export const redirectUri =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/"
+        : "https://bot.phantomknight.tk/";
+export const clientId =
+    process.env.NODE_ENV === "development"
+        ? "839849142925656064"
+        : "838686966387965992";
+export const backendUrl =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3001"
+        : "https://api.phantomknight.tk";
