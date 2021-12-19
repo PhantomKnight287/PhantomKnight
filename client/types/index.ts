6ac12a0881abea94d9aa7f2c506a01153678b8df
PhantomKnight287 <gillgurpal699@gmail.com>
2021-12-19 16:57:38 +0530

    add types

---
+++ client/types/index.ts
@@ -0,0 +1,22 @@
+export type userContext = {
+    avatar: string;
+    discriminator: string;
+    email: string;
+    id: string;
+    username: string;
+};
+export type BackendUserData = {
+    accent_color: number;
+    avatar: string;
+    banner: null;
+    banner_color: string;
+    discriminator: string;
+    email: string;
+    flags: number;
+    id: string;
+    locale: string;
+    mfa_enabled: boolean;
+    public_flags: number;
+    username: string;
+    verified: boolean;
+};
