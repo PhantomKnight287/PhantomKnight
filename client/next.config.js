b2a21ac5a8f25152e319bf63e2f3c5146d55eadf
Gurpal Singh <gillgurpal699@gmail.com>
2021-12-29 16:19:01 +0530

    add scss config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 module.exports = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles/scss")],
+    },
 };
