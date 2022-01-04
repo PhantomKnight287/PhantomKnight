d27025cdb42e1a3653cc42c037b92c766e6734e1
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-04 22:57:36 +0530

    create 404 page

---
+++ client/pages/404.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "styles/scss/pages/404.module.scss";
+import Link from "next/link";
+function Custom404() {
+    return (
+        <div>
+            <Head>
+                <title>Page Not Found</title>
+            </Head>
+            <div className={styles.container}>
+                It looks like you reached DeadEnd.
+                <div>Here to Some Links to Help You</div>
+                <div className={styles.linkContainer}>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Custom404;
