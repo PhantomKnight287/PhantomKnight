12c880269220880687cd112c364e8dc1cad2d786
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-06 23:20:23 +0530

    add gSSP for better SEO

--- client/pages/user/playlist/[userId].tsx
+++ client/pages/user/playlist/[userId].tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useUserState } from "@context/index";
 import { useRouter } from "next/router";
 import styles from "@styles/scss/pages/Playlist.module.scss";
 import { Button } from "@mui/material";
@@ -9,23 +8,24 @@ import axios from "axios";
 import { backendUrl } from "@constants/index";
 import Image from "next/image";
 import { userData as userDataType } from "../../../types";
-function Playlist() {
+import { GetServerSideProps } from "next";
+function Playlist({
+    user,
+}: {
+    user: {
+        songs: { title: string; thumbnail: string }[];
+        message: null | string;
+        user: userDataType;
+    };
+}) {
     const [songs, setSongs] =
         useState<{ title: string; thumbnail: string }[]>();
-    const [message, setMessage] = useState<string>("");
-    const [queriedUser, setQueriedUser] = useState("");
-    const [userData, setUserData] = useState<userDataType>({
-        avatar: "",
-        discriminator: "",
-        username: "",
-    });
-    const user = useUserState();
     const router = useRouter();
     function handleSongRemove(sus: { title: string; thumbnail: string }) {
-        if (!user.id) {
+        if (!(user.user as any).id) {
             return toast.error("You need to Login to Manage the playlist!");
         }
-        if (user.id != router.query.userId) {
+        if ((user.user as any).id != router.query.userId) {
             return toast.error("You Can't Manage this Playlist!");
         }
         axios
@@ -40,83 +40,94 @@ function Playlist() {
             });
     }
     useEffect(() => {
-        if (router.query.userId && router.isReady) {
-            axios
-                .get(`${backendUrl}playlist/${router.query.userId}`)
-                .then((res) => {
-                    if (res.data.message) {
-                        setMessage(res.data.message);
-                    } else {
-                        setSongs(res.data.songs);
-                    }
-                    if (res.data.user) {
-                        setUserData({
-                            avatar: res.data.user.displayAvatarURL,
-                            discriminator: res.data.user.discriminator,
-                            username: res.data.user.username,
-                        });
-                        setQueriedUser(res.data.user.tag);
-                    }
-                })
-                .catch((err) => {
-                    toast.error(err.message);
-                });
-        }
+        setSongs(user.songs);
     }, [router.query.userId]);
     return (
         <>
             <Head>
-                <title>Playlist - {queriedUser}</title>
+                <title>
+                    {user.user && user.user.username
+                        ? `Playlist | ${user.user.username}`
+                        : "Playlist"}
+                </title>
+                <meta
+                    name="og:title"
+                    content={
+                        user.user && user.user.username
+                            ? `Playlist | ${user.user.username}`
+                            : "Playlist"
+                    }
+                />
+                <meta
+                    name="og:description"
+                    content={
+                        user.user && user.user.username
+                            ? `Manage Playlist of ${user.user.username}`
+                            : "Manage Playlist"
+                    }
+                />
+                <meta
+                    name="description"
+                    content={
+                        user.user && user.user.username
+                            ? `Manage Playlist of ${user.user.username}`
+                            : "Manage Playlist"
+                    }
+                />
             </Head>
             <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
 
-            {userData.username && (
+            {user.user && user.user.username && (
                 <div className={styles.userInfoContainer}>
-                    <Image src={userData.avatar} width={128} height={128} />
+                    <Image
+                        src={(user.user as any).displayAvatarURL}
+                        width={128}
+                        height={128}
+                    />
                     <div>
-                        <h3>{userData.username}</h3>
-                        <span>#{userData.discriminator}</span>
+                        <h3>{user.user.username}</h3>
+                        <span>#{user.user.discriminator}</span>
                     </div>
                 </div>
             )}
 
             <div className={styles.mainContentContainer}>
                 {songs && songs.length > 0 ? (
-                    songs.map((song, index) => {
-                        return (
-                            <div
-                                key={index}
-                                className={styles.contentContainer}
-                            >
-                                <Image
-                                    src={`${song.thumbnail.replace(
-                                        "maxresdefault",
-                                        "hqdefault"
-                                    )}`}
-                                    alt="thumbnail"
-                                    width={480}
-                                    height={360}
-                                    loading="lazy"
-                                />
-                                <div>{song.title}</div>
-                                <div className={styles.buttonContainer}>
-                                    <Button
-                                        variant="contained"
-                                        onClick={() => {
-                                            handleSongRemove(song);
-                                        }}
-                                    >
-                                        Remove
-                                    </Button>
+                    songs.map(
+                        (song: { thumbnail: string; title: string }, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className={styles.contentContainer}
+                                >
+                                    <Image
+                                        src={`${song.thumbnail.replace(
+                                            "maxresdefault",
+                                            "hqdefault"
+                                        )}`}
+                                        alt="thumbnail"
+                                        width={480}
+                                        height={360}
+                                        loading="lazy"
+                                    />
+                                    <div>{song.title}</div>
+                                    <div className={styles.buttonContainer}>
+                                        <Button
+                                            variant="contained"
+                                            onClick={() => {
+                                                handleSongRemove(song);
+                                            }}
+                                        >
+                                            Remove
+                                        </Button>
+                                    </div>
                                 </div>
-                            </div>
-                        );
-                    })
+                            );
+                        }
+                    )
                 ) : (
                     <div className={styles.contentContainer}>
-                        {message
-                            ? message
-                            : `It looks like You don't have any playlist created, Go
+                        {`It looks like You don't have any playlist created, Go
                     to discord to create one. If You Have A Playlist and still
                     see this message, Create an issue on`}
                         <a href="https://github.com/PhantomKnight287/Phantomknight">
@@ -128,5 +139,20 @@ function Playlist() {
         </>
     );
 }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const user = await axios
+        .get(`${backendUrl}playlist/${context.query.userId}`)
+        .then((res) => {
+            return res.data;
+        })
+        .catch((err) => {
+            return err.message;
+        });
 
+    return {
+        props: {
+            user,
+        },
+    };
+};
 export default Playlist;
