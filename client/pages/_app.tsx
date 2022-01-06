c1160765b05a9c7f668a1064058364786bbb6e93
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-06 20:33:19 +0530

    add nprogress

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -5,9 +5,13 @@ import Navbar from "../components/Navbar/Navbar";
 import { UserStateProvider } from "../context";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 const queryClient = new QueryClient();
-
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
