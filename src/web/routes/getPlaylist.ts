5cc801e0a03517b4a4b4df970dfe4d91f2c5c65c
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-06 20:24:10 +0530

    add user object in getPlaylist(route)

--- src/web/routes/getPlaylist.ts
+++ src/web/routes/getPlaylist.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { prisma } from "../../prisma";
+import { client } from "../..";
 const router = Router();
 router.get("/playlist/:id", async (req, res) => {
+    const user = client.users.cache.get(req.params.id);
     const playlist = await prisma.playlists.findFirst({
         where: { userId: req.params.id },
     });
     if (playlist && playlist.playList.length > 0) {
-        return res.status(200).send({ songs: playlist.playList, message: null });
+        return res
+            .status(200)
+            .send({ songs: playlist.playList, message: null, user });
     } else {
         return res.status(200).send({
             songs: null,
