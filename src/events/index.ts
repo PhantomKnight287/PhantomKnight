59f9e55205b3116e88dd3b53fb26330aa2807db8
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-08 22:02:12 +0530

    add export statement

--- src/events/index.ts
+++ src/events/index.ts
@@ -3,4 +3,5 @@ import { sendWelcomeMessage } from "./welcomeMessage";
 import { saveEmojis } from "./handleEmojis";
 import { levelling } from "./levelling";
 import { deleteEmojis } from "./guildLeave";
+export * from './messageDelete'
 export { autoMod, sendWelcomeMessage, saveEmojis, levelling, deleteEmojis };
