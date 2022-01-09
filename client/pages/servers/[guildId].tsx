014e623bf7fcaf6dfb98540e33dabddaaf0503e8
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-09 15:16:01 +0530

    add guild dynamic route

---
+++ client/pages/servers/[guildId].tsx
@@ -0,0 +1,13 @@
+import styles from "styles/scss/servers/ManageServer.module.scss";
+import Head from "next/head";
+function ManageServerOption() {
+    return (
+        <div>
+            <Head>
+                <title>Sus</title>
+            </Head>
+        </div>
+    );
+}
+
+export default ManageServerOption;
