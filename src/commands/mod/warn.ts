d70d77740d8558f1ff185fbaf25e79b44188965c
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-09 15:17:08 +0530

    update warn command

--- src/commands/mod/warn.ts
+++ src/commands/mod/warn.ts
@@ -1,6 +1,12 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { Client, CommandInteraction, Permissions } from "discord.js";
+import {
+    CommandInteraction,
+    GuildMember,
+    MessageEmbed,
+    Permissions,
+} from "discord.js";
 import { prisma } from "../../prisma";
+import { client } from "../..";
 module.exports = {
     command: new SlashCommandBuilder()
         .setName("warn")
@@ -14,66 +20,111 @@ module.exports = {
         .addStringOption((reason) => {
             return reason.setName("reason").setDescription("Reason to warn");
         }),
-    async run(interaction: CommandInteraction, client: Client) {
+    async run(interaction: CommandInteraction) {
         await interaction.deferReply({ ephemeral: true });
         const user = interaction.options.getUser("user");
         const reason = interaction.options.getString("reason");
         if (
-            (interaction.member.permissions as any).has([
+            !(interaction.member as unknown as GuildMember).permissions.has([
                 Permissions.FLAGS.KICK_MEMBERS,
                 Permissions.FLAGS.BAN_MEMBERS,
+                Permissions.FLAGS.MANAGE_MESSAGES,
             ])
         ) {
-            const guildRecords = await prisma.warnings.findFirst({
+            return await interaction.editReply({
+                content: "You don't have the necessary permissions!",
+            });
+        }
+        const warnings = await prisma.warn.findFirst({
+            where: {
+                guildId: interaction.guildId,
+            },
+        });
+        if (!warnings) {
+            const warningsArray = [
+                {
+                    moderator: `${interaction.user.username}#${interaction.user.discriminator}`,
+                    reason: reason ? reason : "No Reason Specified",
+                    timestamp: `${new Date().getTime()}`,
+                    warnedUser: `${user.username}#${user.discriminator}`,
+                },
+            ];
+            await prisma.warn.create({
+                data: {
+                    guildId: interaction.guildId,
+                    warnings: warningsArray,
+                },
+            });
+            const dmUser = client.users.cache.get(user.id);
+            const embed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle("You've Been Warned")
+                .setDescription(
+                    `You've been warned in ${interaction.guild.name}`
+                )
+                .addField("Reason", reason ? reason : "No Reason Specified")
+                .setTimestamp()
+                .setAuthor({
+                    name: `${interaction.user.username}#${interaction.user.discriminator}`,
+                    iconURL: interaction.user.displayAvatarURL(),
+                });
+            if (dmUser) {
+                (await dmUser.createDM(true))
+                    .send({ embeds: [embed] })
+                    .catch((error) => {
+                        console.log(error.message);
+                    });
+            }
+            const successEmbed = new MessageEmbed()
+                .setTimestamp()
+                .setColor("GREEN")
+                .setDescription(
+                    `${user.username}#${user.discriminator} has been Warned`
+                );
+            await interaction.editReply({ embeds: [successEmbed] });
+        } else if (warnings) {
+            const newWarnings = {
+                moderator: `${interaction.user.username}#${interaction.user.discriminator}`,
+                reason: reason ? reason : "No Reason Specified",
+                timestamp: `${new Date().getTime()}`,
+                warnedUser: `${user.username}#${user.discriminator}`,
+            };
+            warnings.warnings.push(newWarnings);
+            await prisma.warn.update({
                 where: {
                     guildId: interaction.guildId,
                 },
+                data: {
+                    warnings: warnings.warnings,
+                },
             });
-            if (guildRecords) {
-                const warnings = [
-                    ...(guildRecords as any).warnings,
-                    {
-                        userId: user.id,
-                        reason,
-                    },
-                ];
-                await prisma.warnings.update({
-                    where: {
-                        guildId: interaction.user.id,
-                    },
-                    data: {
-                        warnings,
-                    },
+            const dmUser = client.users.cache.get(user.id);
+            const embed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle("You've Been Warned")
+                .setDescription(
+                    `You've been warned in ${interaction.guild.name}`
+                )
+                .addField("Reason", reason ? reason : "No Reason Specified")
+                .setTimestamp()
+                .setAuthor({
+                    name: `${interaction.user.username}#${interaction.user.discriminator}`,
+                    iconURL: interaction.user.displayAvatarURL(),
                 });
-            } else {
-                const warnings = [
-                    {
-                        userId: user.id,
-                        reason,
-                    },
-                ];
-                await prisma.warnings.create({
-                    data: {
-                        warnings,
-                        guildId: interaction.guildId,
-                    },
-                });
+            if (dmUser) {
+                (await dmUser.createDM(true))
+                    .send({ embeds: [embed] })
+                    .catch((error) => {
+                        console.log(error.message);
+                    });
             }
-
-            try {
-                client.users.cache
-                    .get(user.id)
-                    .send(
-                        `You have been warned in ${
-                            interaction.guild.name
-                        }\n Reason:${reason ? reason : "No Reason Specified"}`
-                    );
-            } catch (error) {
-                console.log(error.message);
-            }
-            await interaction.editReply({
-                content: `Successfully warned ${user.username}`,
-            });
+            const successEmbed = new MessageEmbed()
+                .setTimestamp()
+                .setColor("GREEN")
+                .setDescription(
+                    `${user.username}#${user.discriminator} has been Warned`
+                );
+            await interaction.editReply({ embeds: [successEmbed] });
         }
     },
 };
