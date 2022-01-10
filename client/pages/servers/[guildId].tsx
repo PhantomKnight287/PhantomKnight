b82b58373aa504602d669ff5c13ad9f4c00a2bf1
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-10 22:54:54 +0530

    added nothing in [guildId].tsx tbh

--- client/pages/servers/[guildId].tsx
+++ client/pages/servers/[guildId].tsx
@@ -1,6 +1,12 @@
 import styles from "styles/scss/servers/ManageServer.module.scss";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 function ManageServerOption() {
+    const router = useRouter();
+    useEffect(() => {
+        // console.log(router);
+    }, []);
     return (
         <div>
             <Head>
