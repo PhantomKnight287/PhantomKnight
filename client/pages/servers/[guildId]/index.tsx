0dca5320179f3445c75d1d17fbf7d97f87d82451
Gurpal Singh <gillgurpal699@gmail.com>
2022-01-16 15:19:01 +0530

    create index.tsx for guildId route

---
+++ client/pages/servers/[guildId]/index.tsx
@@ -0,0 +1,54 @@
+import styles from "styles/scss/servers/ManageServer.module.scss";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useUserState } from "@context/userContext";
+import { guild } from "types";
+function ManageServerOption() {
+    const router = useRouter();
+    const { guilds } = useUserState();
+    const [currentGuild, setCurrentGuild] = useState<guild>({} as guild);
+    useEffect(() => {
+        if (!guilds) {
+            // router.push("/servers");
+        }
+        console.log(router.query);
+        guilds?.forEach((eachguild: guild) => {
+            if (eachguild.id == router.query.guildId) {
+                setCurrentGuild(eachguild);
+            }
+        });
+    }, [router.isReady]);
+    return (
+        <div>
+            <Head>
+                <title>
+                    {currentGuild
+                        ? `Configure ${currentGuild.name}'s Settings`
+                        : "Configure Server's Settings"}
+                </title>
+            </Head>
+            <div className={styles.container}>
+                <div
+                    className={styles.contentContainer}
+                    onClick={() => {
+                        router.push(`${router.asPath}/automod`);
+                    }}
+                >
+                    <div className={styles.glassMorph}>
+                        <h3>AutoMod</h3>
+                        <p>Manage AutoMod For Your Server</p>
+                    </div>
+                </div>
+                <div className={styles.contentContainer}>
+                    <div className={styles.glassMorph}>
+                        <h3>Welcome</h3>
+                        <p>Manage Welcome Config For Your Server</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ManageServerOption;
