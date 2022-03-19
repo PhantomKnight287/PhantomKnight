51f4d52226c40e9096a6ddad9b6f383e6cd76622
Gurpal Singh <gillgurpal699@gmail.com>
2022-03-19 23:36:17 +0530

    setup mdx settings

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,20 @@
-/** @type {import('next').NextConfig} */
 const path = require("path");
-module.exports = {
+
+const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+});
+module.exports = withMDX({
+    // Append the default value with md extensions
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
     reactStrictMode: true,
     sassOptions: {
         includePaths: [path.join(__dirname, "styles/scss")],
     },
     images: {
-        domains: ["cdn.discordapp.com","i3.ytimg.com"],
+        domains: ["cdn.discordapp.com", "i3.ytimg.com"],
     },
-};
+});
